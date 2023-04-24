6540550cc329ba0b2e64ad8caa01e4f912fe7628
Mauricio Fernandes <m.ferreira.fernandes@avanade.com>
2023-04-24 11:15:20 -0300

    Initial commit

---
+++ src/test/java/com/apirest/jogorpg/JogoRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.jogorpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogoRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
